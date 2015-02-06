07be8d3f61e367b8fcdb010d98912326d3186930
Katherine Bundy <KB@vlan813-050-024.pioneernet.du.edu>
2015-02-05 21:11:24 -0700

    Nitty Gritty Updates - Ha

--- js/validation.js
+++ js/validation.js
@@ -1,11 +1,13 @@
 function tenDigits() {
-	var phone = $("#phone").val();
+	var phone = $("#phone").val().replace('(','').replace(')','').replace('-','').replace(' ','');
+
 	if (phone.length < 10 || isNaN(phone)){
 		document.getElementById("phone").style.borderColor = "red";
 		document.getElementById("phone").style.backgroundColor = "#f37b7b";
 	} else {
 		document.getElementById("phone").style.borderColor = "green";
 		document.getElementById("phone").style.backgroundColor = "#a0dd98";
+		document.getElementById("phone").value = "(" + phone[0] + phone[1] + phone[2] + ") " + phone[3] + phone[4] + phone[5] + "-" + phone[6] + phone[7] + phone[8] + phone[9];
 		}
 };
 
@@ -19,7 +21,7 @@ function validateForm() {
 	} else if (address.length === 0) {
 		document.getElementById("name").style.backgroundColor = "#a0dd98";
 		document.getElementById("address").style.backgroundColor = "#f37b7b";
-	} else if (phone.length !== 10 || isNaN(phone)) {
+	} else if (phone.length !== 14) {
 		document.getElementById("name").style.backgroundColor = "#a0dd98";
 		document.getElementById("address").style.backgroundColor = "#a0dd98";
 		document.getElementById("phone").style.backgroundColor = "#f37b7b";
@@ -34,9 +36,8 @@ function validateForm() {
 		document.getElementById("address").style.backgroundColor = "#a0dd98";
 		document.getElementById("phone").style.backgroundColor = "#a0dd98";
 		document.getElementById("email").style.backgroundColor = "#a0dd98";
-		var finalPhone = "(" + phone[0] + phone[1] + phone[2] + ") " + phone[3] + phone[4] + phone[5] + "-" + phone[6] + phone[7] + phone[8] + phone[9];
 		alert("Thank you for submitting your information.");
-		console.log('Name = ' + name + '\n' + 'Address = ' + address + '\n' + 'Phone Number = ' + finalPhone + '\n' + 'E-mail = ' + email);
+		console.log('Name = ' + name + '\n' + 'Address = ' + address + '\n' + 'Phone Number = ' + phone + '\n' + 'E-mail = ' + email);
 	}
 };
 
